6032f698ebb73e394e7a83f221e5f0de2941d2bf
PatoGordo <icariusv@gmail.com>
2021-08-22 01:40:10 +0000

    husky, prettier and eslint added to project

--- src/utils/manage-module.ts
+++ src/utils/manage-module.ts
@@ -4,16 +4,16 @@ type Route = {
   name: string;
   path: string;
   component: any;
-}
+};
 
 interface IModule {
   router: {
-    routes: Route[],
-  },
+    routes: Route[];
+  };
 }
 
 export function ManageModule(module: IModule, router: Router) {
-  module.router.routes.forEach(route => {
-    router.addRoute(route)
+  module.router.routes.forEach((route) => {
+    router.addRoute(route);
   });
 }
