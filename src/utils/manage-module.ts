1a6d347f7cf3ba88a113b0e51463503031f5b47e
PatoGordo <icariusv@gmail.com>
2021-08-22 01:42:03 +0000

    type erros fixed

--- src/utils/manage-module.ts
+++ src/utils/manage-module.ts
@@ -1,9 +1,10 @@
+import { DefineComponent } from "vue";
 import { Router } from "vue-router";
 
 type Route = {
   name: string;
   path: string;
-  component: any;
+  component: DefineComponent;
 };
 
 interface IModule {
@@ -12,7 +13,7 @@ interface IModule {
   };
 }
 
-export function ManageModule(module: IModule, router: Router) {
+export function ManageModule(module: IModule, router: Router): void {
   module.router.routes.forEach((route) => {
     router.addRoute(route);
   });
